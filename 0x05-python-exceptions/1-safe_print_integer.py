b09650f657a5576dd92d1a1d3750fdeac59ceafb
Mak655 <mekdemyoseph@gmail.com>
2022-10-10 16:30:51 +0000

    king

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    """ Returns True if value has been correctly printed
+(it means the value is an integer)"""
     try:
         print("{:d}".format(value))
-    except:
+        return True
+    except (ValueError, TypeError):
         return False
-    return True
