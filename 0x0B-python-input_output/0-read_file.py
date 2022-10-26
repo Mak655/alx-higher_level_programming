797cf715235604514df303fed5796da660e819a0
Mak655 <mekdemyoseph@gmail.com>
2022-10-26 14:53:04 +0000

    king

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Contain a single function that read files"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """Reads and prints content of file"""
+    """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
