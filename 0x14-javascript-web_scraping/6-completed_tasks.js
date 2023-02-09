d21533c7e33c52d0a4b36cfc7305e71c05961436
Mak655 <mekdemyoseph@gmail.com>
2023-02-09 17:28:10 +0000

    king

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
-/**
- * Script that computes
- * the number of tasks completed by user id
- */
 
 const request = require('request');
-const process = require('process');
 const url = process.argv[2];
 
-request.get(url, (err, _resp, body) => {
-    if (err === null) {
-        const data = JSON.parse(body);
-        const userIds = [...new Set(data.map(todo => todo.userId))];
-        const completedTasksData = {};
-        userIds.forEach((id) => {
-            const completedTasks = data.filter(todo => todo.userId === id && todo.completed);
-            if (completedTasks.length <= 0) {
-                return;
-            }
-            completedTasksData[id] = completedTasks.length;
-        });
-        console.log(completedTasksData);
-    } else {
-        console.log(err);
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
     }
+  });
+  console.log(tasksCompleted);
 });
