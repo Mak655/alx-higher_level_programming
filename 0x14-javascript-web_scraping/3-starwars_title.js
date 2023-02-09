f6332a29c8b6b65027249142b150c21aaada2260
Mak655 <mekdemyoseph@gmail.com>
2023-02-09 17:22:44 +0000

    king

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-/**
- * Script that prints the title of a Star Wars movie
- * where the episode number matches a given integer
- */
+
 const request = require('request');
-const process = require('process');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const episodNumber = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${episodNumber}`;
-
-request.get(url, (err, _resp, body) => {
-    if (err === null) {
-        const data = JSON.parse(body);
-        console.log(data.title);
-    } else {
-        console.log(err);
-    }
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
