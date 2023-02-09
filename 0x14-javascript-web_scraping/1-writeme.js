f02027157c19ccb579cbfac233a7e574acb8fd14
Mak655 <mekdemyoseph@gmail.com>
2023-02-09 17:12:59 +0000

    king

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-/**
- * Script that writes a string to a file
- */
 
-const process = require('process');
 const fs = require('fs');
-
-const filePath = process.argv[2];
+const filename = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', err => {
-    if (err) {
-        console.log(err);
-    }
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
 });
