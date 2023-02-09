bbd6ab8b8c0b4e34070c691e82a00fb0fdba763c
Mak655 <mekdemyoseph@gmail.com>
2023-02-09 17:11:06 +0000

    king

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-/**
- * Script that reads from a file
- */
-const process = require('process');
+
 const fs = require('fs');
+const filename = process.argv[2];
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+fs.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
